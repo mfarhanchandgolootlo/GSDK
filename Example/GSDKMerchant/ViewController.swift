b38602cd8708b5d3a114115940d4036fb15f8539
Muhammad Farhan <muhammad.farhan@golootlo.pk>
2024-10-25 17:01:20 +0500

    fix build 0.0.11

--- Example/GSDKMerchant/ViewController.swift
+++ Example/GSDKMerchant/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
-
+import GSDKMerchant
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        testion()
+    }
+    
+    func testion()
+    {
+        var urlString  = "https://webview.golootlo.pk/home?data=rDBeANsIebUducq4MQRoRUaf3mHneuByBdP4uFKjjiSlRmNKonPKc1y%2BwYQlo1PFx0vSoJnlk7f%2B3pZAmhbDR0yq5RelQ8UFSppp%2BBZ%2BWRYFpqcM69H%2BoRNmD2h8VKTMnTNjOVxGoFthvlMr4wd7bXxZ3f97AmqHnd%2F8oYcdlggnD08RBkxNHDK7FRyJC5gBteoH66Usemr%2FsnPvmqXRGZ4Xv2XkQ2zR0Vus6vyV6XHeN3hhEmWIxWMfFSvDIhujMr6H9uJpDfvSk%2BF7mbaiL0NmS0NafTYXY01%2FcPBVwdIJ70xLLxvZv41PXcFH0k%2B9X7aPAC5uWyRVVzcAe0d4cQ%3D%3D&appversion=2.1.7"
+        
+        if let url = URL(string: urlString)
+        {
+            let merchant = GolootloWebController(webURL: url, delegate: self)
+            self.present(merchant, animated: true) {
+                print("asdfasdfsafd")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,3 +36,34 @@ class ViewController: UIViewController {
 
 }
 
+
+extension ViewController: GolootloEventDelegate
+{
+    func golootlo(event: String) {
+        print("asdfsadf")
+    }
+    
+    func golootloViewDidLoad(animated: Bool) {
+        print("asdfsadf")
+    }
+    
+    func golootloViewWillAppear(_ animated: Bool) {
+        print("asdfsadf")
+    }
+    
+    func golootloViewDidAppear(_ animated: Bool) {
+        print("asdfsadf")
+    }
+    
+    func golootloViewDidDisappear(_ animated: Bool) {
+        print("asdfsadf")
+    }
+    
+    func golootloViewWillDisappear(_ animated: Bool) {
+        print("asdfsadf")
+    }
+    
+    func golootloWillMoveFromParent() {
+        print("asdfsadf")
+    }
+}
