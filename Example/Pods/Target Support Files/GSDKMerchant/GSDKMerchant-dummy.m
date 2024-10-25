66f6f1dd7be9fc8dacafdf8070c9384bee5899d2
Muhammad Farhan <muhammad.farhan@golootlo.pk>
2024-10-25 13:38:04 +0500

    iniital Comit

---
+++ Example/Pods/Target Support Files/GSDKMerchant/GSDKMerchant-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GSDKMerchant : NSObject
+@end
+@implementation PodsDummy_GSDKMerchant
+@end
---
+++ Example/Pods/Target Support Files/Pods-GSDKMerchant_Tests/Pods-GSDKMerchant_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GSDKMerchant_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_GSDKMerchant_Tests
+@end
