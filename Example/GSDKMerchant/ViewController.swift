a11077b88310d47fe562675f22d8c7058231debe
Muhammad Farhan <muhammad.farhan@golootlo.pk>
2024-10-29 18:04:38 +0500

    commit code

--- Example/GSDKMerchant/ViewController.swift
+++ Example/GSDKMerchant/ViewController.swift
@@ -10,20 +10,24 @@ import UIKit
 import GSDKMerchant
 class ViewController: UIViewController {
 
+    let dataValue = "UserId=NBP&FirstName=TEST&LastName=USER&Phone=923360824990&Password=YMcxt4S1I3ZpG6dqsvxt"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        howToUseThisFunc()
     }
     
-    func howToUseThisFunc()
+    @IBAction func actionButtonShowControllerUsingPresnet()
     {
-        let dataValue = "UserId=NBP&FirstName=TEST&LastName=USER&Phone=923360824990&Password=YMcxt4S1I3ZpG6dqsvxt"
-        
         let merchant = GolootloWebController(baseURL: "https://webview-staging.golootlo.pk/home?", delegate: self, dataObject: dataValue, appversion: "2.1.7", hideCross: false, crossAlignemtn: 1, pemfile: "Golootlo-Staging-Public-Key")
-//        self.present(merchant, animated: true) {
-//            print("loaded controller")
-//        }
+        
+        self.present(merchant, animated: true) {
+            print("loaded controller")
+        }
+    }
+    
+    @IBAction func actionButtonShowControllerUsingNavigaControl()
+    {
+        let merchant = GolootloWebController(baseURL: "https://webview-staging.golootlo.pk/home?", delegate: self, dataObject: dataValue, appversion: "2.1.7", hideCross: false, crossAlignemtn: 1, pemfile: "Golootlo-Staging-Public-Key")
         
         self.navigationController?.pushViewController(merchant, animated: true)
     }
